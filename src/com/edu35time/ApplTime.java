2923ec2aa2135dc66a9ad0965c62336103823b0e
volodymyrzubyk <vovazubykv@gmail.com>
2023-09-26 18:02:55 +0200

    message updated

--- src/com/edu35time/ApplTime.java
+++ src/com/edu35time/ApplTime.java
@@ -66,7 +66,7 @@ public class ApplTime {
 				.plus(-3, ChronoUnit.CENTURIES);
 		int day = past.getDayOfMonth();
 		DayOfWeek dayweek = past.getDayOfWeek();
-		System.out.println("\tpast = " + past + "  day = " + day + "  dayweek = " + dayweek);
+		System.out.println("\npast = " + past + "  day = " + day + "  dayweek = " + dayweek);
 		// */
 		/*-
 		// Get the current date and time
